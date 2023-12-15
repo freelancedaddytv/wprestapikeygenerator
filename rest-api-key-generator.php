a82611028085a2b08637087f42fa7c8d2902c819
Freelance Daddy TV <jlandicho83@gmail.com>
2023-12-16 03:59:13 +0800

    Update rest-api-key-generator.php

--- rest-api-key-generator.php
+++ rest-api-key-generator.php
@@ -3,7 +3,7 @@
  * Plugin Name: REST API Key Generator
  * Description: Generates and displays a REST API key for administrators.
  * Version: 1.0
- * Author: Your Name
+ * Author: Freelance Daddy
  */
 
 // Add a menu item under the "Settings" menu in the admin dashboard
@@ -23,12 +23,22 @@ add_action('admin_menu', 'rest_api_key_menu');
 // Function to generate and display the API key
 function generate_rest_api_key() {
     if (current_user_can('manage_options')) {
-        $api_key = wp_generate_password(32); // Generate a random 32-character key
+        $api_key = get_option('rest_api_key'); // Get the stored API key
+
+        // Check if the form has been submitted
+        if (isset($_POST['save_api_key'])) {
+            $new_api_key = sanitize_text_field($_POST['api_key']); // Get the user-inputted API key
+            update_option('rest_api_key', $new_api_key); // Save the new API key to the database
+            $api_key = $new_api_key; // Update the displayed API key
+        }
 
         echo '<div class="wrap">';
         echo '<h2>REST API Key</h2>';
+        echo '<form method="post">';
         echo '<p>Here is your REST API key:</p>';
-        echo '<input type="text" value="' . esc_attr($api_key) . '" readonly="readonly" style="width: 100%; font-size: 14px; padding: 10px; margin-bottom: 20px;">';
+        echo '<input type="text" name="api_key" value="' . esc_attr($api_key) . '" style="width: 100%; font-size: 14px; padding: 10px; margin-bottom: 20px;" readonly="readonly">';
+        echo '<input type="submit" name="save_api_key" value="Save API Key">';
+        echo '</form>';
         echo '</div>';
     }
 }
